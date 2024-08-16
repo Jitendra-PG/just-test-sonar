f04f06d85de00ec8bf8fe510d47ab2cff00499dd
Jitendra-PG <jitendra-accenture@peacegeeks.org>
2024-08-16 13:47:18 -0400

    Update another_deserial.js

--- another_deserial.js
+++ another_deserial.js
@@ -3,8 +3,8 @@
 //safeLoadAll and jsyaml.safeLoad are vulnerable if DEFAULT_FULL_SCHEMA is used 
 const jsyaml = require("js-yaml");
 
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
 app.post('/store/:id', function(req, res) {
   let data;
   let unsafeConfig = { schema: jsyaml.DEFAULT_FULL_SCHEMA };
